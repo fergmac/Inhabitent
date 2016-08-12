83333e62b6aa037695a717c1320369bea798d6f3
fergmac <f.macconnell@gmail.com>
2016-08-12 11:56:53 -0700

    first commit

---
+++ themes/inhabitent/front-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+
+		<h1>hello world</h1>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
+
