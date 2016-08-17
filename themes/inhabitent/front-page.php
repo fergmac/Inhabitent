3a2d4399b05c7e008382848f12c9532d5be3e5a6
fergmac <f.macconnell@gmail.com>
2016-08-17 15:52:45 -0700

    flexed shop stuff and inhabitent journal on front page.

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -9,7 +9,8 @@ get_header(); ?>
 
 	<div id="primary" cla	ss="content-area">
 		<main id="main" class="site-main" role="main">
-
+	<h2>Shop Stuff</h2>
+	<div class="shop-stuff-container">
 
 		<?php  //shop stuff on front page
 			$arg = array( 'taxonomy' => 'product-type', 'hide_empty' => true);
@@ -17,39 +18,38 @@ get_header(); ?>
 			$terms = get_terms( $arg ); 
     	?>
 		<?php foreach ( $terms as $term ) : ?>
+		<div class="product-type-block">
+			<a href=" <?php get_term_link($term); ?> "><?php echo $term->name. ' Stuff'
+			?></a> 
+			
+			<img src="<?php echo get_template_directory_uri().'/images/product-type-icons/'.$term->slug. '.svg';?>" alt = ""/>
 
-		<a href=" <?php get_term_link($term); ?> "><?php echo $term->name. ' Stuff'
-		?></a> 
+			<p><?php echo $term->description ?></p>
+		</div>
 		
-		<img src="<?php echo get_template_directory_uri().'/images/product-type-icons/'.$term->slug. '.svg';?>" alt = ""/>
-
-		<p><?php echo $term->description ?></p>
-
-		
-
-
 		<?php endforeach; ?>
-
-
-
+	</div>
+	<h2>Inhabitent Journal</h2>
+	<div class="inhabitent-journal">
 		<?php //inhabitent journal posts on front page
 			$args = array( 'post_type' => 'post', 'posts_per_page'   => 3 );
 
    			$blog_posts = get_posts( $args ); // returns an array of posts
    		?>
    		<?php foreach ( $blog_posts as $post ) : setup_postdata( $post ); ?>
+   		<div class="inhabitent-journal-block">
+		<ul>
+	   		<li><?php the_post_thumbnail(); ?></li> 
 
-   		<?php the_post_thumbnail(); ?> 
+	   		<li><p><?php the_date(); ?></p></li>
 
-   		<p><?php the_date(); ?></p>
+			<li><h1><?php the_title(); ?></h1></li>
 
-		<h1><?php the_title(); ?></h1>
-
-		<a href="<?php the_permalink(); ?>"><div class="read-entry">read entry</div></a>
-		
-	
+			<li><a href="<?php the_permalink(); ?>"><div class="read-entry">read entry</div></a></li>
+		</ul>	
+		</div>
    		<?php endforeach; wp_reset_postdata(); ?>
-
+	</div>
 
 
 
--- themes/inhabitent/page-about.php
+++ themes/inhabitent/page-about.php
@@ -2,7 +2,7 @@
 /**
  * Template part for displaying about page.
  * Template name: About Page
- * @package RED_Starter_Theme
+ * @package Inhabitent_Theme
  */
 
 get_header(); ?>
@@ -10,10 +10,10 @@ get_header(); ?>
 	<div id="primary" class="about-content-area">
 		<main id="main" class="site-main" role="main">
 			<div class="about-page-hero">About</div>
-
+			<div class="about-info">
 			<?php echo CFS()->get( 'our_story' ); ?>
 			<?php echo CFS()->get( 'our_team' ); ?>
-
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 	
