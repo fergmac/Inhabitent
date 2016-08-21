bd31a8d74356e7d30ab84c88731f4537bcb2b4b7
fergmac <f.macconnell@gmail.com>
2016-08-21 16:36:38 -0700

    styled taxonomy page.

--- themes/inhabitent/taxonomy-product-type.php
+++ themes/inhabitent/taxonomy-product-type.php
@@ -1,34 +1,75 @@
-<?php
+	<?php
 /**
- * The template for displaying all pages.
- * Template name:
+ * The template for displaying archive pages.
+ * Template name: Shop Stuff
  * @package Inhabitent_Theme
  */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="product-content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+		<?php if ( have_posts() ) : ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="page-header">
+			<div class="taxonomy-wrapper">
+				<div class="product-list-style">
+					
+				<?php  //shop stuff on front page
+				$arg = array( 'taxonomy' => 'product-type', 'hide_empty' => true);
+
+				$terms = get_terms( $arg ); 
+				?>
+				 <h1 class="taxonomy-title">
+       			<?php single_term_title();?>
+       			</h1>
+					
+				<?php
+					
+					the_archive_description( '<div e="taxonomy-description">', '</div>' );
+				?>
+				</div>
+			</div>
+			</header><!-- .page-header -->
+
+			
+			<div class="archive-product-grid">
+			
+			<?php while ( have_posts() ) : the_post(); ?>
+			<div class="single-product-block">
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>">
 					<header class="entry-header">
-						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+						
+							<div class="thumbnail-wrapper">
+							<a href="<?php echo get_permalink(); ?> ">
+						<?php if ( has_post_thumbnail() ) : ?>
+							<?php the_post_thumbnail( 'large' ); ?>
+						<?php endif; ?>
+							</a>
+							</div>
+						<div class="product-info">
+						<?php the_title(); ?>
+
+
+
+						</div>
+						
+
 					</header><!-- .entry-header -->
 
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php
-						wp_link_pages( array(
-							'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-							'after'  => '</div>',
-							) );
-							?>
-						</div><!-- .entry-content -->
-					</article><!-- #post-## -->
+					
+				</article><!-- #post-## -->
+			</div>
+			<?php endwhile; ?>
+			</div>
+			<?php the_posts_navigation(); ?>
+			
+		<?php else : ?>
 
-			<?php endwhile; // End of the loop. ?>
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
